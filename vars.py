121de8f6e6c230f061bb809f45e19a272732c513
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-07-14 20:06:01 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@ import os
 
 # fill all veriables all are important
 
-B_TOKEN = ""
-API = 
-API_HASH = ""
-BOT_USERNAME = ""
-ownerid = 
-DB_URI = ""
+B_TOKEN = "7434890598:AAEQy1H3bCaF8KW2u8bsC4DrKjjdTQToRqY"
+API = 26360430
+API_HASH = "4f7076fc90ac313f59d935d4e421efed"
+BOT_USERNAME = "Accept1_2tbot"
+ownerid = 7189414187
+DB_URI = "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority"
