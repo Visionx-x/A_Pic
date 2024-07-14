d7b63ef7ecc80a338e44a9fbfd5d48c547193d55
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-07-14 19:52:22 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,16 +2,9 @@ import os
 
 # fill all veriables all are important
 
-
-# your bot token
-B_TOKEN = "6512679486:AAEJ7wPwa8GCVqr1bL_OHy2i4JSHpRo9bYM"
-#your api id
-API = 10248430
-# your api hash
-API_HASH = "42396a6ff14a569b9d59931643897d0d"
-# your bot username without @ like this ultron2_robot
-BOT_USERNAME = "Dup96_bot"
-# your owner id
-ownerid = 6056049745
-#mongo db url
-DB_URI = "mongodb+srv://haris12:haris12@cluster0.u8qzvr4.mongodb.net/?retryWrites=true&w=majority"
+B_TOKEN = ""
+API = 
+API_HASH = ""
+BOT_USERNAME = ""
+ownerid = 
+DB_URI = ""
